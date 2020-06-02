c7d1ffd0f9616eab2081b9fca099105d4d043eaf
Ben Gamari <ben@smart-cactus.org>
2020-06-02 17:15:27 -0400

    testsuite: Add tests for #18227
    T18227A is the original issue which gave rise to the ticket and depends
    upon bytestring. T18227B is a minimized reproducer.

---
+++ testsuite/tests/codeGen/should_compile/T18227B.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE GADTs #-}
+
+module T18227B where
+
+import Unsafe.Coerce
+
+test1 :: UnsafeEquality Int Char -> IO ()
+test1 hi = print "hello"
+{-# NOINLINE test1 #-}
+
+test2 :: IO ()
+test2 =
+  case unsafeEqualityProof :: UnsafeEquality Int Char of
+    proof@UnsafeRefl -> test1 proof
